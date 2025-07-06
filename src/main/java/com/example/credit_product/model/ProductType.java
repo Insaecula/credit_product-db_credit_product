dc31c1b449cd1043d6b95f08cb1c3aeb03079175
insaecula <morozzzik228@gmail.com>
2025-07-06 17:15:50 +0300

    model

--- src/main/java/com/example/credit_product/model/ProductType.java
+++
@@ -1,9 +0,0 @@
-package com.example.credit_product.model;
-
-import java.util.UUID;
-
-public record ProductType(
-        UUID id,
-        String code,  // например, "SAVING"
-        String name   // например, "Сберегательный счёт"
-) {}
--- src/main/java/com/example/credit_product/model/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.credit_product.model;
-
-import java.util.UUID;
-
-public record User(
-        UUID id,
-        String name
-) {}
